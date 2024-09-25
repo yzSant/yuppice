0ed26a77e2953c2b3be93f6df64a7af54ac245f0
yzSant <lcssantana1805@gmail.com>
2024-09-25 13:26:38 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var linkedin = GetElementById("integrante 1");
+var linkedin = GetElementById("integrante 2");
+var linkedin = GetElementById("integrante 3");
+var linkedin = GetElementById("integrante 4");
+var linkedin = GetElementById("integrante 5");
