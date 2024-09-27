2ca52789075508392e40026d89fe8ea0c91b12b6
yzSant <lcssantana1805@gmail.com>
2024-09-27 20:26:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-var linkedin = GetElementById("integrante 1");
-var linkedin = GetElementById("integrante 2");
-var linkedin = GetElementById("integrante 3");
-var linkedin = GetElementById("integrante 4");
-var linkedin = GetElementById("integrante 5");
+
